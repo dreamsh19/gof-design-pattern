9609edf65639ab64a79e6b3751fa2a769c82b8b0
dreamsh19 <dreamsh19@gmail.com>
2025-01-27 18:08:03 +0900

    Maze Game 데모 앱을 위한 Spring initializer 설정

---
+++ maze-game-app/src/main/java/com/dremash19/mazegameapp/MazeGameAppApplication.java
@@ -0,0 +1,13 @@
+package com.dremash19.mazegameapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazeGameAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MazeGameAppApplication.class, args);
+    }
+
+}
